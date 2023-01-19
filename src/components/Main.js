592a7a6bba709008478af69334271146bed0e69c
Steven Bryant <stevelad_83@yahoo.com>
2023-01-19 18:06:27 -0500

    images and data rendering

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import usePokemon from '../hooks/Pokemon.js';
+import PokeCard from './PokeCard.js';
 
 export default function Main() {
   const pokemon = usePokemon();
-  return <div>{pokemon}</div>;
+  return (
+    <div>
+      {pokemon.map((poke) => (
+        // <p key={poke._id}>{poke.pokemon}</p>
+
+        <PokeCard key={poke._id} {...poke} />
+      ))}
+    </div>
+  );
 }
---
+++ src/components/PokeCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function PokeCard({ pokemon, id, ability_1, url_image, type_1, type_2 }) {
+  return (
+    <>
+      <div className="poke-card">
+        <p>{id}</p>
+        <h1>{pokemon}</h1>
+        <img src={url_image}></img>
+        <p>{ability_1}</p>
+        <p>{type_1}</p>
+        <p>{type_2}</p>
+      </div>
+    </>
+  );
+}
