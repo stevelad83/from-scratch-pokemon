3f2191cf831766b333a08f4facb568f3cb80d213
Steven Bryant <stevelad_83@yahoo.com>
2023-01-20 14:29:36 -0500

    removed Select from App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main.js';
-import Select from './components/Select.js';
 
 function App() {
   return (
     <div className="App">
       <Main />
-      <Select />
     </div>
   );
 }
--- src/components/Select.js
+++ src/components/Select.js
@@ -4,17 +4,13 @@ export default function Select({ types, loading }) {
   console.log('types', types);
 
   return (
-    <>
-{//if....then...}
-
-      <select className="select">
-        <option value="all">All</option>
-        {types.map((type, i) => (
-          <option key={i} value={type}>
-            {type}
-          </option>
-        ))}
-      </select>
-    </>
+    <select className="select">
+      <option value="all">All</option>
+      {types.map((type, i) => (
+        <option key={i} value={type}>
+          {type}
+        </option>
+      ))}
+    </select>
   );
 }
