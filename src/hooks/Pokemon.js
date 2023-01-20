566fb9cf1faf13661742d639f148655a61c7e795
Steven Bryant <stevelad_83@yahoo.com>
2023-01-19 20:30:37 -0500

    adding handleChange

--- src/hooks/Pokemon.js
+++ src/hooks/Pokemon.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { fetchInitialPokemon } from '../services/fetchPokemon.js';
+import { fetchInitialPokemon, fetchPokemon, fetchTypes } from '../services/fetchPokemon.js';
 
 export default function usePokemon() {
   const [pokemon, setPokemon] = useState([]);
+  const [types, setTypes] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -15,5 +16,19 @@ export default function usePokemon() {
     fetchData();
   }, []);
 
-  return pokemon;
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchTypes();
+      setTypes(data);
+    };
+    fetchData();
+  }, []);
+
+  const handleTypeChange = async (type) => {
+    setLoading(true);
+    const data = await fetchPokemon(type);
+    setPokemon(data);
+    setLoading(false);
+  };
+  return { pokemon, types, handleTypeChange, loading };
 }
--- src/services/fetchPokemon.js
+++ src/services/fetchPokemon.js
@@ -15,3 +15,9 @@ export async function fetchTypes() {
   console.log('data', data);
   return data;
 }
+
+export async function fetchPokemon(type) {
+  const response = await fetch(`https://alchemy-pokedex.herokuapp.com/api/pokedex/?type=${type}`);
+  const data = await response.json();
+  return data.results;
+}
